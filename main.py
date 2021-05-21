5811d523aad581f1a85ee2d17610e21b4548ceb3
Marcelo de Castro <marcelo.castro@engenharia.ufjf.br>
2021-05-20 22:53:35 -0400

    updating gui

--- main.py
+++ main.py
@@ -7,21 +7,25 @@
 try:
     # for Python2
     from Tkinter import * ## notice capitalized T in Tkinter 
+    from Tkinter import ttk
     from Tkinter import tkFileDialog
     from Tkinter import tkMessageBox
 except ImportError:
     # for Python3
     from tkinter import *   ## notice lowercase 't' in tkinter here
+    from tkinter import ttk
     import tkinter.filedialog as tkFileDialog
     import tkinter.messagebox as tkMessageBox
 # ----- Importing sys module:
 import sys, os, time
 homedir= os.getcwd()
 # ----- Adding paths for new modules:
-srcdir = homedir+ "/src"
+srcdir = homedir + "/src"
 auxdir = homedir + "/aux"
+imgdir = homedir + "/img" # create img dir path
 sys.path.insert(1, srcdir)
 sys.path.insert(2, auxdir)
+sys.path.insert(3, imgdir) # add img dir to home path
 # ----- Importing auxiliary functions:
 import directory_functions
 import psse2mo
@@ -65,9 +69,32 @@ def frompsse():
 # Description: Python GUI interface for model translation tool.
 #==================================================================================  
 root = Tk()
+# ----- Root Frame:
 root.title("Model Translation Tool")
-app = Canvas(root, width=800, height=400,bg='black')
-app.pack(expand=YES, fill=BOTH)
+root.geometry("350x800")
+
+
+# ----- Right frame with additional info:
+rightframe = Frame(master=root, width=400, height=800, bg="white")
+rightframe.pack(side=RIGHT)
+
+introtxt = "Model Translation Tool"
+copytxt = "AlsetLab and New York Power Authority \n 2021"
+canvas = Canvas(rightframe, width = 400, height = 800, bg="white")
+canvas.pack(fill = BOTH, expand = True)
+icon = PhotoImage(file = imgdir+'/icon.png')
+icon = icon.zoom(4)
+icon = icon.subsample(9)
+canvas.create_image(190, 235, image=icon)
+canvas.create_text(175, 50, text = introtxt, font = ('Times New Roman', 18), justify = 'center', fill='black')
+canvas.create_text(175, 750, text = copytxt, font = ('Times New Roman', 12), justify = 'center', fill='black')
+canvas.update
+
+# ----- Left frame with text being displayed
+# leftframe = Frame(master=root, width=1242, height=800, bg="white")
+# leftframe.pack(side=LEFT)
+
+# ----- Menu
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
 newmenu = Menu(menubar,tearoff=0)
